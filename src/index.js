6cc57f580b44065207abeb6f464c6771ca52b9e2
pzx521521 <pzx521521@qq.com>
2025-01-12 21:37:41 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import PostalMime from "postal-mime";
+
+async function streamToArrayBuffer(stream, streamSize) {
+  let result = new Uint8Array(streamSize);
+  let bytesRead = 0;
+  const reader = stream.getReader();
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) {
+      break;
+    }
+    result.set(value, bytesRead);
+    bytesRead += value.length;
+  }
+  return result;
+}
+
+export default {
+  async email(event, env, ctx) {
+    const rawEmail = await streamToArrayBuffer(event.raw, event.rawSize);
+    const parser = new PostalMime.default();
+    const parsedEmail = await parser.parse(rawEmail);
+    console.log("Mail subject: ", parsedEmail.subject);
+    console.log("Mail message ID", parsedEmail.messageId);
+    console.log("HTML version of Email: ", parsedEmail.html);
+    console.log("Text version of Email: ", parsedEmail.text);
+    if (parsedEmail.attachments.length == 0) {
+      console.log("No attachments");
+    } else {
+      parsedEmail.attachments.forEach((att) => {
+        console.log("Attachment: ", att.filename);
+        console.log("Attachment disposition: ", att.disposition);
+        console.log("Attachment mime type: ", att.mimeType);
+        console.log("Attachment size: ", att.content.byteLength);
+      });
+    }
+    const { from, to } = message;
+    const subject = message.headers.get("subject")
+    const redisKey = `${from}|${to}`;  // Redis 键名
+    const redisUrl = "https://modern-baboon-27609.upstash.io"
+    const headers = { "Authorization": "Bearer AWvZAAIjcDE3ZjlkZTlkZDZhYWY0ZmVhYTRlNzJhYzEyOTdjMDBiZHAxMA" }
+    const ttl = 60 * 15;  // 设置 15 分钟的过期时间（单位：秒）
+    // const headersObj = Object.fromEntries(message.headers);
+    const body = { "subject": subject, "body": message.body, "text": message.text, "html": message.html };  // 这是邮件正文部分
+    const response = await fetch(`${redisUrl}/set/${redisKey}?ex=${ttl}`, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(body)
+    });
+    const resp = await response.text()
+    console.log(body)
+    await event.forward("pzx521521@qq.com")
+  },
+};
