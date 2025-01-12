40575665b81d50a901b60b7e13ee6b3cb70fd937
pzx521521 <pzx521521@qq.com>
2025-01-12 22:50:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -49,20 +49,20 @@ export default {
     const rawEmail = await streamToArrayBuffer(message.raw, message.rawSize);
     const parser = new PostalMime();
     const parsedEmail = await parser.parse(rawEmail);
-    console.log("Mail subject: ", parsedEmail.subject);
-    console.log("Mail message ID", parsedEmail.messageId);
-    console.log("HTML version of Email: ", parsedEmail.html);
-    console.log("Text version of Email: ", parsedEmail.text);
-    if (parsedEmail.attachments.length == 0) {
-      console.log("No attachments");
-    } else {
-      parsedEmail.attachments.forEach((att) => {
-        console.log("Attachment: ", att.filename);
-        console.log("Attachment disposition: ", att.disposition);
-        console.log("Attachment mime type: ", att.mimeType);
-        console.log("Attachment size: ", att.content.byteLength);
-      });
-    }
+    // console.log("Mail subject: ", parsedEmail.subject);
+    // console.log("Mail message ID", parsedEmail.messageId);
+    // console.log("HTML version of Email: ", parsedEmail.html);
+    // console.log("Text version of Email: ", parsedEmail.text);
+    // if (parsedEmail.attachments.length == 0) {
+    //   console.log("No attachments");
+    // } else {
+    //   parsedEmail.attachments.forEach((att) => {
+    //     console.log("Attachment: ", att.filename);
+    //     console.log("Attachment disposition: ", att.disposition);
+    //     console.log("Attachment mime type: ", att.mimeType);
+    //     console.log("Attachment size: ", att.content.byteLength);
+    //   });
+    // }
     const { from, to } = message;
     const subject = message.headers.get("subject")
     const redisKey = `${from}|${to}`;  // Redis 键名
